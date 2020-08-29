e1e909e032c74893759e3a895792b0933cd3c603
victor fernandez gayan <victor.fernandez.gayan@gmail.com>
2020-08-29 19:52:14 +0200

    quitar opcion perfil y controlar el logout

---
+++ module/actions/course-subjects/logout.php
@@ -0,0 +1,6 @@
+<?php
+    include ('../../../service/mysqlConection.php');
+    session_start();
+    session_destroy();
+    header("Location: ../../../start.php");
+?>
--- service/mysqlConection.php
+++ service/mysqlConection.php
@@ -2,7 +2,7 @@
     session_start();
     $host="localhost";
     $username="root";
-    $password="123";
+    $password="";
     $db_name="schoolDB";
 
     $link = mysqli_connect($host,$username,$password,$db_name);
